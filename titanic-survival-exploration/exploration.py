6fd145702ce39297296c517d5e1f976c9f93f3f1
pedronovaes <pedronovaesmelo@gmail.com>
2018-06-06 20:35:57 -0300

    Adding new model to predict young male

--- titanic-survival-exploration/exploration.py
+++ titanic-survival-exploration/exploration.py
@@ -34,13 +34,28 @@ def predictions_1(data):
     return pd.Series(predictions)
 
 
+# Model with two features:
+# - predict a passenger survived if they are female
+# - predict a passenger survived if they are male and younger than 10
+def predictions_2(data):
+    predictions = []
+    for _, passenger in data.iterrows():
+        if (passenger["Sex"] == "female"):
+            predictions.append(1)
+        elif (passenger["Sex"] == "male" and passenger["Age"] < 10):
+            predictions.append(1)
+        else:
+            predictions.append(0)
+    return pd.Series(predictions)
+
+
 if __name__ == '__main__':
     # Loading dataset
     in_file = "titanic_data.csv"
     full_data = pd.read_csv(in_file)
 
     # Print the first few entries of the RMS Titanic data
-    # display(full_data.head())
+    display(full_data.head())
 
     # Removing "Survived" variable from the dataset
     # We will use these outcomes as our prediction targets
@@ -54,4 +69,7 @@ if __name__ == '__main__':
     print(accuracy_score(outcomes, predictions))
 
     predictions = predictions_1(data)
+    print(accuracy_score(outcomes, predictions))
+
+    predictions = predictions_2(data)
     print(accuracy_score(outcomes, predictions))
