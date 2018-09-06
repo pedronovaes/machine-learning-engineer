6a7c2183332407ee06afc2671d63244809abc34f
pedronovaes <pedronovaesmelo@gmail.com>
2018-09-05 22:32:14 -0300

    Creating function to plot relationships between variables using matplotlib

--- charity-ml/finding_donors.py
+++ charity-ml/finding_donors.py
@@ -1,19 +1,27 @@
 # Importing libraries necessary for this project
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from IPython.display import display
+
+
+def plot_relationships(x_axis, hue, data, y_axis=None):
+    sns.countplot(x=x_axis, y=y_axis,  hue=hue, data=data)
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
     # Loading dataset
     in_file = 'census.csv'
-    full_data = pd.read_csv(in_file)
+    data = pd.read_csv(in_file)
 
-    display(full_data.head())
+    display(data.head())
 
     # Making some data exploration
-    n_records = len(full_data)
-    n_greater_50k = len(full_data[full_data.income == '>50K'])
-    n_at_most_50k = len(full_data[full_data.income == '<=50K'])
+    n_records = len(data)
+    n_greater_50k = len(data[data.income == '>50K'])
+    n_at_most_50k = len(data[data.income == '<=50K'])
     greater_percent = n_greater_50k / n_records * 100
 
     # Print the results
@@ -21,3 +29,5 @@ if __name__ == '__main__':
     print("Individuals making more than $50,000: {}".format(n_greater_50k))
     print("Individuals making at most $50,000: {}".format(n_at_most_50k))
     print("Percentage of individuals making more than $50,000: {0:.2f}%".format(greater_percent))
+
+    plot_relationships(x_axis="age", hue="income", data=data)
