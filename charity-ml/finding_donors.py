de9573c24a97b5cd46bef6a0bb201bc600e3c557
pedronovaes <pedronovaesmelo@gmail.com>
2018-09-07 14:37:24 -0300

    Modularizing the preprocessing step

--- charity-ml/finding_donors.py
+++ charity-ml/finding_donors.py
@@ -14,6 +14,36 @@ def plot_relationships(x_axis, hue, data, y_axis=None):
     plt.show()
 
 
+def preprocessing(data):
+    # Transforming skewed continuous features and splitting the data into features and target label
+    income_raw = data['income']
+    features_raw = data.drop('income', axis=1)
+
+    # Log-transform the skewed features
+    skewed = ['capital-gain', 'capital-loss']
+    features_log_transformed = pd.DataFrame(data=features_raw)
+    features_log_transformed[skewed] = features_log_transformed[skewed].apply(lambda x: np.log(x + 1))
+
+    # Normalizing numerical features
+    # Initialize a scaler, then apply it to the features
+    scaler = MinMaxScaler()
+    numerical = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+
+    features_log_minmax_transform = pd.DataFrame(data=features_log_transformed)
+    features_log_minmax_transform[numerical] = scaler.fit_transform(features_log_transformed[numerical])
+
+    # Applying one-hot encoding to non-numerical features
+    features_final = pd.get_dummies(features_log_minmax_transform)
+
+    # Encode the 'income_raw' data to numerical values
+    income = income_raw.apply(lambda x: 1 if x == '>50K' else 0)
+
+    encoded = list(features_final.columns)
+    print("{} total features after one-hot encoding".format(len(encoded)))
+
+    return features_final, income
+
+
 if __name__ == '__main__':
     # Loading dataset
     in_file = 'census.csv'
@@ -35,32 +65,7 @@ if __name__ == '__main__':
 
     # plot_relationships(x_axis="age", hue="income", data=data)
 
-    # Preparing the data
-    # --- Transforming skewed continuous features
-    # --- Split the data into features and target lagel
-    income_raw = data['income']
-    features_raw = data.drop('income', axis=1)
-
-    # --- Log-transform the skewed features
-    skewed = ['capital-gain', 'capital-loss']
-    features_log_transformed = pd.DataFrame(data=features_raw)
-    features_log_transformed[skewed] = features_raw[skewed].apply(lambda x: np.log(x + 1))
-
-    # --- Initialize a scaler, then apply it to the features
-    scaler = MinMaxScaler()  # default=(0, 1)
-    numerical = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
-
-    features_log_minmax_transform = pd.DataFrame(data=features_log_transformed)
-    features_log_minmax_transform[numerical] = scaler.fit_transform(features_log_transformed[numerical])
-
-    # --- Applying one-hot encoding to non-numerical features
-    features_final = pd.get_dummies(features_log_minmax_transform)
-
-    # --- Encode the 'income_raw' data to numerical values
-    income = income_raw.apply(lambda x: 1 if x == '>50K' else 0)
-
-    encoded = list(features_final.columns)
-    print("{} total features after one-hot encoding".format(len(encoded)))
+    features_final, income = preprocessing(data)
 
     # Shuffle and Split data
     X_train, X_test, y_train, y_test = train_test_split(features_final, income, test_size=0.2, random_state=0)
