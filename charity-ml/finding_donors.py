9066a3379f573560c576c4358f398692947fc649
pedronovaes <pedronovaesmelo@gmail.com>
2018-09-09 11:20:55 -0300

    Creating a Training and Predicting Pipeline

--- charity-ml/finding_donors.py
+++ charity-ml/finding_donors.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from IPython.display import display
+from time import time
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import fbeta_score, accuracy_score
 
 
 def plot_relationships(x_axis, hue, data, y_axis=None):
@@ -59,6 +61,44 @@ def naive_predictor_performance(income, n_records):
     # Calculate F-score for beta = 0.5
     fscore = (1 + 0.5 ** 2) * precision * recall / ((0.5 ** 2 * precision) + recall)
     print("Naive Predictor: [Accuracy score: {:.4f}, F-score: {:.4f}]".format(accuracy, fscore))
+
+
+def train_predict(learner, sample_size, X_train, y_train, X_test, y_test):
+    '''
+    inputs:
+        - learner: the learning algorithm to be trained and predicted on
+        - sample_size: the size of samples (number) to be drawn from training set
+        - X_train: features training set
+        - y_train: income training set
+        - X_test: features testing set
+        - y_test: income testing set
+    '''
+    results = {}
+
+    # Training data
+    start = time()
+    learner.fit(X_train[:sample_size], y_train[:sample_size])
+    end = time()
+    results['train_time'] = end - start
+
+    # Predictions
+    start = time()
+    predictions_test = learner.predict(X_test)
+    predictions_train = learner.predict(X_train[:300])
+    end = time()
+    results['pred_time'] = end - start
+
+    # Computing accuracy on the first 300 training samples and on the test set
+    results['acc_train'] = accuracy_score(y_train[:300], predictions_train)
+    results['acc_test'] = accuracy_score(y_test, predictions_test)
+
+    # Computing F-score on the frist 300 training samples and on the test set
+    results['f_train'] = fbeta_score(y_train[:300], predictions_train, 0.5)
+    results['f_test'] = fbeta_score(y_test, predictions_test, 0.5)
+
+    print("{} trained on {} samples.".format(learner.__class__.__name__, sample_size))
+
+    return results
 
 
 if __name__ == '__main__':
