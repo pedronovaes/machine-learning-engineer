992df779c8132fc3f346b3d12704b7d5866f1bcf
pedronovaes <pedronovaesmelo@gmail.com>
2018-09-20 20:44:58 -0300

    Performing some KMeans executions with different number of clusters

--- movie_ratings/recomendation_system.py
+++ movie_ratings/recomendation_system.py
@@ -15,32 +15,34 @@ if __name__ == '__main__':
     ratings = pd.read_csv('ml-latest-small/ratings.csv')
     print(ratings.head())
 
-    print('The dataset contains: ', len(ratings), ' ratings of ', len(movies), 'movies.')
+    print('The dataset contains: {} ratings of {} movies.'.format(len(ratings), len(movies)))
 
-    # Romance vs Scifi
-    # Calculate the average rating of romance and scifi movies
+    # Taking a subset of users and seeing the respective rating of romance and scifi movies
     genre_ratings = helper.get_genre_ratings(ratings, movies, ['Romance', 'Sci-Fi'], ['avg_romance_rating', 'avg_scifi_rating'])
     print(genre_ratings.head())
 
     # Removing people who like both scifi and romance
     biased_dataset = helper.bias_genre_rating_dataset(genre_ratings, 3.2, 2.5)
-    print('Number of records: ', len(biased_dataset))
+    print('Number of records: {}'.format(len(biased_dataset)))
     print(biased_dataset.head())
+
+    # Plotting the dataset
+    # helper.draw_scatterplot(biased_dataset['avg_scifi_rating'],'Avg scifi rating', biased_dataset['avg_romance_rating'], 'Avg romance rating')
 
     # Turning the dataset into a list
     X = biased_dataset[['avg_scifi_rating', 'avg_romance_rating']].values
 
     # Creating an instance of KMeans to find two clusters
     kmeans_1 = KMeans(n_clusters=2)
-    predictions = kmeans_1.fit_predict(X)
-    print('predictions 1: ', predictions, '\n')
+    predictions_1 = kmeans_1.fit_predict(X)
+    # helper.draw_clusters(biased_dataset, predictions_1)
 
-    # Creating an instance of KMeans to find three clusters
+    # Three clusters
     kmeans_2 = KMeans(n_clusters=3)
     predictions_2 = kmeans_2.fit_predict(X)
-    print('predictions 2: ', predictions_2, '\n')
+    # helper.draw_clusters(biased_dataset, predictions_2)
 
-    # Creating an instance of KMeans to find four clusters
+    # Four clusters
     kmeans_3 = KMeans(n_clusters=4)
     predictions_3 = kmeans_3.fit_predict(X)
-    print('predictions 3: ', predictions_3, '\n')
+    helper.draw_clusters(biased_dataset, predictions_3)
