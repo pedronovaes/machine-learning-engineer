822dfbb7d5db2b4aef6ee3f7ee624e2ef0e3d9f4
pedronovaes <pedronovaesmelo@gmail.com>
2018-09-21 01:18:39 -0300

    Adding function to create a cluster

--- movie_ratings/recomendation_system.py
+++ movie_ratings/recomendation_system.py
@@ -24,6 +24,13 @@ def plot_elbow_method(possible_k_values, errors_per_k):
     ax.yaxis.grid(True, which='both')
 
 
+def making_cluster(X, n_clusters):
+    kmeans = KMeans(n_clusters=n_clusters)
+    predictions = kmeans.fit_predict(X)
+
+    return predictions
+
+
 if __name__ == '__main__':
     # Import the Movies dataset
     movies = pd.read_csv('ml-latest-small/movies.csv')
@@ -50,27 +57,54 @@ if __name__ == '__main__':
     # Turning the dataset into a list
     X = biased_dataset[['avg_scifi_rating', 'avg_romance_rating']].values
 
-    # Creating an instance of KMeans to find two clusters
-    kmeans_1 = KMeans(n_clusters=2)
-    predictions_1 = kmeans_1.fit_predict(X)
+    # Creating two clusters
+    predictions_1 = making_cluster(X, 2)
     # helper.draw_clusters(biased_dataset, predictions_1)
 
     # Three clusters
-    kmeans_2 = KMeans(n_clusters=3)
-    predictions_2 = kmeans_2.fit_predict(X)
+    predictions_2 = making_cluster(X, 3)
     # helper.draw_clusters(biased_dataset, predictions_2)
 
     # Four clusters
-    kmeans_3 = KMeans(n_clusters=4)
-    predictions_3 = kmeans_3.fit_predict(X)
+    predictions_3 = making_cluster(X, 4)
     # helper.draw_clusters(biased_dataset, predictions_3)
 
     # Using Elbow Method to find best K value
-    possible_k_values = range(2, len(X) + 1, 5)
-    errors_per_k = [helper.clustering_errors(k, X) for k in possible_k_values]
-    plot_elbow_method(possible_k_values, errors_per_k)
+    # possible_k_values = range(2, len(X) + 1, 5)
+    # errors_per_k = [helper.clustering_errors(k, X) for k in possible_k_values]
+    # plot_elbow_method(possible_k_values, errors_per_k)
 
     # Seven clusters
-    kmeans_4 = KMeans(n_clusters=7)
-    predictions_4 = kmeans_4.fit_predict(X)
+    predictions_4 = making_cluster(X, 7)
     helper.draw_clusters(biased_dataset, predictions_4)
+
+    '''
+    # Taking a subset of users and seeing the respective rating of romance, scifi and action movies
+    biased_dataset_3_genres = helper.get_genre_ratings(ratings, movies, ['Romance', 'Sci-Fi', 'Action'], ['avg_romance_rating', 'avg_scifi_rating', 'avg_action_rating'])
+    biased_dataset_3_genres = helper.bias_genre_rating_dataset(biased_dataset_3_genres, 3.2, 2.5).dropna()
+    print('Number of records: {}'.format(len(biased_dataset_3_genres)))
+    print(biased_dataset_3_genres.head())
+
+    X_with_action = biased_dataset_3_genres[['avg_scifi_rating', 'avg_romance_rating', 'avg_action_rating']].values
+
+    kmeans_5 = KMeans(n_clusters=7)
+    predictions_5 = kmeans_5.fit_predict(X_with_action)
+    # helper.draw_clusters(biased_dataset_3_genres, predictions_5)
+
+    # Movie-level Clustering
+    # Merge the two tables then pivot so we have Users X Movies dataframe
+    ratings_title = pd.merge(ratings, movies[['movieId', 'title']], on='movieId')
+    user_movie_ratings = pd.pivot_table(ratings_title, index='userId', columns= 'title', values='rating')
+
+    print('dataset dimensions: ', user_movie_ratings.shape, '\n\nSubset example:')
+    print(user_movie_ratings.iloc[:6, :10])
+
+    n_movies = 30
+    n_users = 18
+    most_rated_movies_users_selection = helper.sort_by_rating_density(user_movie_ratings, n_movies, n_users)
+
+    print('dataset dimensions: ', most_rated_movies_users_selection.shape)
+    print(most_rated_movies_users_selection.head())
+
+    helper.draw_movies_heatmap(most_rated_movies_users_selection)
+    '''
